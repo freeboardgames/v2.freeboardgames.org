3d9ed95c0adfd8f7acfa2319c7c173effd090240
freeboardgames <robot@FreeBoardGames.org>
2022-08-21 05:32:23 +0000

    See https://github.com/freeboardgames/FreeBoardGames.org/commit/0ea7ec71829f5a9f117a2a2fb846c24783e5d106 from refs/heads/master

--- _next/static/_mHm-PznrDqnIbWo-dr4G/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F[lang]","\u002F[lang]\u002F[playVerb]\u002F[gameCode]","\u002F[lang]\u002F[playVerb]\u002F[gameCode]\u002Flocal"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/x8egGSj6FtYstyJaIkJHT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[lang]","\u002F[lang]\u002F[playVerb]\u002F[gameCode]","\u002F[lang]\u002F[playVerb]\u002F[gameCode]\u002Flocal","\u002F[lang]\u002Fabout"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
